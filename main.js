a0346454c9bcd6fb10643129b264d3adb84e9a48
Brian Ferrell <endenizen@gmail.com>
2013-02-23 14:25:36 -0800

    Hook up search box, add read me

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 (function() {
+  var geocoder;
   var map;
   var layerl0;
+  var marker;
+
 
   function initialize() {
+
+    geocoder = new google.maps.Geocoder();
 
     map = new google.maps.Map(document.getElementById('map-canvas'), {
       center: new google.maps.LatLng(37.808441719910775, -122.20826827734379),
@@ -20,6 +25,31 @@
       templateId: 2
     });
 
+    var button = document.getElementById('search_button');
+    button.onclick = function() {
+      var search = document.getElementById('search_input').value;
+      codeAddress(search);
+      return false;
+    };
+  }
+
+  function codeAddress(address) {
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        map.setZoom(15);
+        if (!marker) {
+          marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+          });
+        } else {
+          marker.setPosition(results[0].geometry.location);
+        }
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
   }
   google.maps.event.addDomListener(window, 'load', initialize);
 })();
